99df0833886973e14202a91be695d75b4cc3e7c9
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-09-04 18:17:20 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { theme } from '@/styles/theme'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Dashgo</title>
+      </Head>
+
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Dashgo</h1>
+}
+
+export default Home
