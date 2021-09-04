a90993afa890f25a2e15e5c0ba44fc1214f54f95
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-09-04 19:46:03 -0300

    feat: add Sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,16 @@
-import { Header } from '@/components/Header'
+import { Flex } from '@chakra-ui/react'
 
-const Dashboard = () => <Header />
+import { Header } from '@/components/Header'
+import { Sidebar } from '@/components/Sidebar'
+
+const Dashboard = () => (
+  <Flex flexDir="column" h="100vh">
+    <Header />
+
+    <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+      <Sidebar />
+    </Flex>
+  </Flex>
+)
 
 export default Dashboard
